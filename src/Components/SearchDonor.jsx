bae038c942dc7d0e9964485d8e0fcdbf7fcfef14
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2024-02-07 07:56:43 +0530

    search completed

--- src/Components/SearchDonor.jsx
+++ src/Components/SearchDonor.jsx
@@ -9,16 +9,19 @@ const SearchDonor = () => {
         changeData({...data,[e.target.name]:e.target.value})
     )
 
+const [response,changeResponse]=useState([])
+
 
     const submitValue=()=>{
         console.log(data)
         axios.post("http://localhost:4000/search",data).then(
             (response)=>{
                 console.log(response)
-                if(response.data.length===0){
+                if(response.data.data.length===0){
                     alert("Donor Not Found...")
+                    changeResponse([])
                 }else{
-                    alert("Donor data found")
+                    changeResponse(response.data.data)
                     
                 }
             }
@@ -28,7 +31,7 @@ const SearchDonor = () => {
 
   return (
     <>
-      <Header/>
+      <Header />
       <div className="container">
         <div className="row">
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -37,12 +40,51 @@ const SearchDonor = () => {
                 <label htmlFor="" className="form-label">
                   Blood Group
                 </label>
-                <input type="text" className="form-control" name="bloodGroup" value={data.bloodGroup} onChange={readValue} />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="bloodGroup"
+                  value={data.bloodGroup}
+                  onChange={readValue}
+                />
               </div>
               <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <button className="btn btn-success" onClick={submitValue}>Search</button>
+                <button className="btn btn-success" onClick={submitValue}>
+                  Search
+                </button>
               </div>
             </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">First Name</th>
+                  <th scope="col">Last Name</th>
+                  <th scope="col">Mobile Number</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                    response.map(
+                        (value,index)=>{
+                            return <tr>
+                                <th scope="row">{index+1}</th>
+                                <td>{value.firstName}</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                              </tr>
+                            
+
+                        }
+                    )
+                }
+               
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
