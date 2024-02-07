13a58ba6cdb9d2c7a602fbf4f9f70130f9a07df8
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2024-02-07 07:02:05 +0530

    searchComponent created but not completed

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
 <ViewAllDonors/>
 <DeleteDonor/>
 <SearchDonor/>
+
     </div>
   );
 }
--- src/Components/SearchDonor.jsx
+++ src/Components/SearchDonor.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header';
+import axios from 'axios';
 
 const SearchDonor = () => {
+    const [data, changeData] = useState({ bloodGroup: "" });
+
+    const readValue=(e)=>(
+        changeData({...data,[e.target.name]:e.target.value})
+    )
+
+
+    const submitValue=()=>{
+        console.log(data)
+        axios.post("http://localhost:4000/search",data).then(
+            (response)=>{
+                console.log(response)
+                if(response.data.length===0){
+                    alert("Donor Not Found...")
+                }else{
+                    alert("Donor data found")
+                    
+                }
+            }
+        )
+    }
+
+
   return (
     <>
       <Header/>
@@ -13,10 +37,10 @@ const SearchDonor = () => {
                 <label htmlFor="" className="form-label">
                   Blood Group
                 </label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" name="bloodGroup" value={data.bloodGroup} onChange={readValue} />
               </div>
               <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <button className="btn btn-success">Delete</button>
+                <button className="btn btn-success" onClick={submitValue}>Search</button>
               </div>
             </div>
           </div>
