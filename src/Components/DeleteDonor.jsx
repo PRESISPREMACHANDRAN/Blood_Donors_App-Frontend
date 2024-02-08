2887a759b6990d5d1d668aefc7011f6862a3e76f
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2024-02-08 20:56:57 +0530

    delete donor api connected and worked

--- src/Components/DeleteDonor.jsx
+++ src/Components/DeleteDonor.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header';
+import axios from 'axios';
 
 const DeleteDonor = () => {
 
@@ -8,12 +9,32 @@ const DeleteDonor = () => {
         "phoneNumber":""
     })
 
+    const readValue=(e)=>{
+      changeData({...data,[e.target.name]:e.target.value})
+    }
+   
+
+    const submitValue=()=>{
+      console.log(data)
+      axios.post("http://localhost:4000/delete",data).then(
+        (response)=>{
+          console.log(response)
+
+          if(response.data.status==="success"){
+            alert("Donor deleted....")
+          }else{
+            alert("Donor does not exist....")
+          }
+        }
+      )
+    }
+
     
 
 
   return (
     <>
-      <Header/>
+      <Header />
       <div className="container">
         <div className="row">
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -22,10 +43,16 @@ const DeleteDonor = () => {
                 <label htmlFor="" className="form-label">
                   Phone Number
                 </label>
-                <input type="text" className="form-control" />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="phoneNumber"
+                  value={data.phoneNumber}
+                  onChange={readValue}
+                />
               </div>
               <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <button className="btn btn-success">Delete</button>
+                <button className="btn btn-success" onClick={submitValue}>Delete</button>
               </div>
             </div>
           </div>
