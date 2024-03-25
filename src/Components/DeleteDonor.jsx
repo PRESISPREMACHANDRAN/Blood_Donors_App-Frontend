00ca08f4127dedf83dc2d84b59e2abe120f16aa5
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2024-03-25 10:19:17 +0530

    changes

--- src/Components/DeleteDonor.jsx
+++ src/Components/DeleteDonor.jsx
@@ -47,6 +47,7 @@ const DeleteDonor = () => {
                   name="phoneNumber"
                   value={data.phoneNumber}
                   onChange={readValue}
+                  required
                 />
               </div>
               <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
